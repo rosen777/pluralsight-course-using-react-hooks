75d32622c2995c08071baa6b16d786ead3eb087a
Rosen Toshev <rtoscheff@gmail.com>
2021-08-23 02:53:52 +0100

    Added the SpeakerDetails page with favorites, filter and sort logic.

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const InputElement = () => {
-  return <input placeholder="Enter Some Text" />;
+  const random_boolean = Math.random() >= 0.5;
+  const [isLoading, setIsLoading] = useState(random_boolean === true);
+
+  // if (random_boolean === true) {
+  //   const [isLoading, setIsLoading] = useState(true);
+  // } else {
+  //   const [isLoading, setIsLoding] = useState(false);
+  // }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  })
+
+  return isLoading ? <div>Loading...</div> : <input placeholder="Enter Some Text" />;
 };
 
 export default InputElement;
---
+++ 03-More-React-Hooks-useContext-useReducer-useCallback-useMemo/clip02-React-Hooks-eslint-and-Usage/src/SpeakerDetail.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ImageToggleOnScroll from './ImageToggleOnScroll';
+
+function SpeakerDetail({
+    id,
+    firstName,
+    lastName,
+    favorite,
+    bio,
+    onHeartFavoriteHandler,
+}) {
+    return (
+        <div className="cart col-4 cardmin">
+            <ImageToggleOnScroll className="card-img-top"
+                                primaryImg={`/static/speakers/bw/Speaker-${id}.jpg`}
+                                secondaryImg={`/static/speakers/Speaker-${id}.jpg`}
+                                alt={`${firstName} ${lastName}`} />
+            <div className="card-body">
+                <h4 className="card-title">
+                    <button data-sessionid={id}
+                            className={favorite ? 'heartredbutton' : 'heartdarkbutton'}
+                            onClick={(e) => {
+                                onHeartFavoriteHandler(e, !favorite);
+                            }}
+                    />
+                    <span>
+                        {firstName} {lastName}
+                    </span>
+                </h4>
+                <span>
+                    {bio}
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default SpeakerDetail;
